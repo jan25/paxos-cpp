51c48454888bc0b042102c2acc117ffe7e89cd86
sri kailash <sri.gandebathula@booking.com>
2018-12-26 15:36:23 +0100

    message header

---
+++ Message.h
@@ -0,0 +1,38 @@
+class Message
+{
+public:
+	Message();
+	~Message();
+};
+
+
+class ProposeMessage: public Message{
+public:
+	int id;
+	ProposeMessage(int Id);
+	~ProposeMessage();
+	void test();
+};
+
+class AcceptMessage: public Message{
+public:
+	int id;
+	int value;
+	AcceptMessage(int Id, int Value);
+	~AcceptMessage();
+};
+
+class PromisedMessage: public Message{
+public:
+	int id;
+	PromisedMessage(int Id);
+	~PromisedMessage();	
+};
+
+class AcceptedMessage: public Message{
+public:
+	int id;
+	int value;
+	AcceptedMessage(int Id, int Value);
+	~AcceptedMessage();
+};
